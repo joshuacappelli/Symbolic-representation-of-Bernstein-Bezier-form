92f64a2961eafa3fa2d4a65b4539ce78f1846ea3
Josh Cappelli <joshua@cappelliconnect.com>
2023-12-24 16:51:42 +0100

    function for adding bezier curves

---
+++ b1add.py
@@ -0,0 +1,37 @@
+from sympy.tensor.array.expressions import ArraySymbol
+from sympy import symbols
+
+def bb1add(dg, b1, b2, b1_cof, b2_cof):
+    # the length of multipliers
+    b1_summands = len(b1_cof)
+    b2_summands = len(b2_cof)
+
+    # Because of the property of ArraySymbol, we need to store the element of ArraySymbol into list to do the computation
+    b1_local = [b1[i, 0] for i in range(dg + 1)]
+    b2_local = [b2[i, 0] for i in range(dg + 1)]
+
+    print(b1_local)
+    print(b2_local)
+    # The algorithm
+    out = [0] * (dg + 1)
+    print("this is the out: ")
+    print(out)
+    for i in range(dg + 1):
+        # Computate b1
+        for j in range(b1_summands):
+            out[i] += b1_local[i] * b1_cof[j]
+        # Computate b2
+        for k in range(b2_summands):
+            out[i] += b2_local[i] * b2_cof[k]
+
+    return out
+
+dg = 1  # the degree of b1 and b2
+b1 = ArraySymbol("b1", (dg + 1, 1))
+b2 = ArraySymbol("b2", (dg + 1, 1))
+# Manual input of multipliers
+b1_cof = [-1]
+b2_cof = [2,3,5]
+
+bb = bb1add(dg, b1, b2, b1_cof, b2_cof)
+print(bb)
