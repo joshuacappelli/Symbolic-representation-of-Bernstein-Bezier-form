464cc1b5aa9388a9049473913752f8a702bc6f28
Josh Cappelli <joshua@cappelliconnect.com>
2024-01-13 14:22:17 -0500

    simplified bb1add

--- bb1add.py
+++ bb1add.py
@@ -3,23 +3,15 @@ import sympy
 from sympy import symbols
 
 def bb1add(dg, b1, b2, b1_cof, b2_cof):
-    # the length of multipliers
-    b1_summands = len(b1_cof)
-    b2_summands = len(b2_cof)
-
-    # Because of the property of ArraySymbol, we need to store the element of ArraySymbol into list to do the computation
-    b1_local = b1
-    b2_local = b2
-
     # The algorithm
     out = [0] * (dg + 1)
     for i in range(dg + 1):
         # Computate b1
-        for j in range(b1_summands):
-            out[i] += b1_local[i] * b1_cof[j]
+        for j in range(len(b1_cof)):
+            out[i] += b1[i] * b1_cof[j]
         # Computate b2
-        for k in range(b2_summands):
-            out[i] += b2_local[i] * b2_cof[k]
+        for k in range(len(b2_cof)):
+            out[i] += b2[i] * b2_cof[k]
 
     return out
 
